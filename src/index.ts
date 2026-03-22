253420f77ee5f6bbdd20d92b617bc97ccb3f4e3a
nadyasav <nadyasav28@gmail.com>
2026-03-23 02:53:43 +0300

    feat: implement put and delete endpoints

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,13 @@ import { API_PRODUCTS } from './constants';
 import dotenv from 'dotenv';
 import { validate as isValidUUID } from 'uuid';
 import Fastify from 'fastify'
-import z from 'zod';
+import { ProductData, productSchema } from './schema';
 
 dotenv.config();
 
 const PORT = Number(process.env.PORT) || 3000;
 
 const products: Record<UUID, Product> = {};
-
-const productSchema = z.object({
-  name: z.string().min(1),
-  description: z.string().min(1),
-  price: z.number().positive(),
-  category: z.string().min(1),
-  inStock: z.boolean(),
-});
 
 const server = Fastify({
   logger: true
@@ -44,7 +36,7 @@ server.get<{ Params: { productId: string } }>(`${API_PRODUCTS}/:productId`, asyn
   return reply.code(200).send({ data: product });
 });
 
-server.post<{ Body: z.infer<typeof productSchema> }>(API_PRODUCTS, async (request, reply) => {
+server.post<{ Body: ProductData }>(API_PRODUCTS, async (request, reply) => {
   const body = productSchema.safeParse(request.body);
 
   if(!body.success) {
@@ -59,6 +51,46 @@ server.post<{ Body: z.infer<typeof productSchema> }>(API_PRODUCTS, async (reques
   products[productId] = newProduct;
 
   return reply.status(201).send({ data: newProduct });
+});
+
+server.put<{ Params: { productId: string }; Body: ProductData }>(`${API_PRODUCTS}/:productId`, async (request, reply) => {
+  const { productId } = request.params;
+
+  if (!isValidUUID(productId)) {
+    return reply.code(400).send({ error: 'Invalid product id format' });
+  }
+
+  const product = products[productId as UUID];
+  if(!product) {
+    return reply.code(404).send({ error: 'Product not found' });
+  }
+
+  const body = productSchema.safeParse(request.body);
+  if(!body.success) {
+    return reply.status(400).send({ error: 'Invalid product data' });
+  }
+
+  products[productId as UUID] = {
+    id: productId as UUID,
+    ...body.data,
+  };
+
+  return reply.status(200).send({ data: products[productId as UUID] });
+});
+
+server.delete<{ Params: { productId: string } }>(`${API_PRODUCTS}/:productId`, async (request, reply) => {
+  const { productId } = request.params;
+
+  if (!isValidUUID(productId)) {
+    return reply.code(400).send({ error: 'Invalid product id format' });
+  }
+
+  if(!products[productId as UUID]) {
+    return reply.code(404).send({ error: 'Product not found' });
+  }
+
+  delete products[productId as UUID];
+  return reply.status(204).send();
 });
 
 server.setNotFoundHandler((_request, reply) => {
